d1afa7df15344705682ef630c41bf619ec91e1fb
Alomgir <70021915+Alomgir27@users.noreply.github.com>
2022-09-24 22:40:36 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import 'firebase/compat/storage';
     
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBKVon02Kd4Wp-X7pqVE1eBJ2xqJJZEB1E",
-    authDomain: "friendsvoice-83a55.firebaseapp.com",
-    projectId: "friendsvoice-83a55",
-    storageBucket: "friendsvoice-83a55.appspot.com",
-    messagingSenderId: "585705831310",
-    appId: "1:585705831310:web:3c7adcdfe790f830cbc2df",
-    measurementId: "G-D5ZG791WLG"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
   
 
